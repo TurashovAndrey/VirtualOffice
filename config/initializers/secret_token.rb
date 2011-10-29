ebc2790d78a9a4b974cf9bed381b9d063ecfad1c
Andrey T <turashov@mail.ru>
2011-10-29 17:47:56 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VirtualOffice::Application.config.secret_token = '240b74bcea27604a272c13e5537a7de9d9e185d595ead2416aa5c114f557f2bcf61d756e4bc7564f19f1047caba70d39ff83c3c60072825197debb24985e25c1'
