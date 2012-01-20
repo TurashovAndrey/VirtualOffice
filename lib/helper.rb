23eaf9cc5b96e5f83b43d63bb0a110994643f34c
Andrey T <turashov@mail.ru>
2012-01-20 17:37:32 +0200

    Features support

--- lib/helper.rb
+++ lib/helper.rb
@@ -3,6 +3,7 @@ module Helper
   require 'envolve_chat'
 
   def self.get_chat_script(user)
+
     if (user!=nil)
       @firstname = user.first_name=="" ? user.email: user.first_name
       @envolve_html_code = EnvolveChat::ChatRenderer.get_html("50879-2vO9DDq6kmqbiIViHl2rntDi9mFADrIY",
@@ -11,13 +12,13 @@ module Helper
         :last_name => user.last_name,
         :is_admin => false,
         :groups => [{:id => user.company.url_base, :name => user.company.name},{:id => "Default", :name => "Default chat"}],
-        :strings => "Sound : 'Привет!'"
+        :strings => "TurnOff : 'Выключить',TurnOnTooltip:'Включить',Sound:'Звук',LogOutTitleText:'Выйти',LoggingOut:'Выйти из чата'"
       })
     else
       @envolve_html_code = EnvolveChat::ChatRenderer.get_html("50879-2vO9DDq6kmqbiIViHl2rntDi9mFADrIY",
       {
         :is_admin => false,
-      })
+        :strings => "TurnOff : 'Выключить',TurnOnTooltip:'Включить',Sound:'Звук',LogOutTitleText:'Выйти',LoggingOut:'Выйти из чата'"      })
     end
 
     @envolve_html_code
